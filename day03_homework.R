4f6d6ee5d16e2adb1ec463b7a8f2c6a89ca7afa5
joelnitta <joelnitta@gmail.com>
2023-10-15 10:49:06 +0900

    Initial commit

---
+++ day03_homework.R
@@ -0,0 +1,61 @@
+# DAY 3 HOMEWORK
+#
+# Name:
+#
+# 1. What are the values after each statement in the following?
+#
+# Type in the value after the comment, like this:
+# mass = 40
+# (I have done the first one for you)
+
+mass <- 160                # mass = 160
+height <- 6                # height =
+mass <- mass / 2           # mass =
+height <- height * 30      # height =
+bmi <- height / mass       # bmi =
+
+# 2. The following code has an error. Fix it by editing the code:
+
+weight_lb <- 160
+weight_kg <- weight_Lb / 2.2
+
+# 3. What is the value of z after running the following code?
+x <- 2
+y <- 3
+z <- x + y
+x <- 5
+
+
+
+# 4. The sqrt() function calculates the square root of a number.
+# Use the sqrt() function to calculate the square root of 25.
+
+
+
+# 5. The seq() function generates sequences of numbers.
+# Its arguments include "from" (the starting number), "to"
+# (the ending number), and "by" (how much to add
+# between each number)
+#
+# Use seq() make the following sequence of numbers:
+# 2, 4, 6, 8, 10
+
+
+
+# 6. Make a vector of logical values (TRUE and FALSE), then make vector of
+# character values. Next, combine the two vectors. What is the type of
+# vector that you obtain?
+#
+# HINT: use c() to make vectors
+# HINT: characters are always written with quotation marks
+
+
+
+
+# 7. R includes some pre-defined vectors, like `letters`:
+letters
+
+# Use subsetting and the seq() function to subset every other letter starting
+# with "a"
+#
+# HINT: Use `seq()` inside the square brackets
